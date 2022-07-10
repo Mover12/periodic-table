9a532091e35b59ccbd33099cbc0bc3039c2af72d
Mover12 <mixail.eli.2416@gmail.com>
2022-07-10 20:12:09 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 export default function App() {
   return (
-    <div className="App">
-      <div className="hello">Hello</div>
-    </div>
+    <div className="App"></div>
   );
 }
