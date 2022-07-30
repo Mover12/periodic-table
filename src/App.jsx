24bf9b20624e9d2bcf58374acb87ff28f342962c
Mover12 <mixail.eli.2416@gmail.com>
2022-07-30 10:29:40 +0300

    c

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      Hello! This is periodic-table! 
+    </div>
   );
 }
